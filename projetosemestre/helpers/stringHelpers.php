0686b5273ded9a1373f9baed2d2479186a26594f
BrunoOsio <62727801+BrunoOsio@users.noreply.github.com>
2023-10-16 17:43:11 -0300

    Add files via upload

---
+++ projetosemestre/helpers/stringHelpers.php
@@ -0,0 +1,33 @@
+<?php
+
+function capitalize($string)
+{
+    $words = explode(" ", $string);
+
+    foreach ($words as $index => $word) {
+        $words[$index] = ucfirst($word);
+    }
+
+    return implode(" ", $words);
+}
+
+function getFirstName($fullName)
+{
+    $nameParts = explode(" ", $fullName);
+    return $nameParts[0];
+}
+
+function getFullLastName($name)
+{
+    $nameParts = explode(" ", $name);
+    $stringsAfterFirstName = [];
+
+    foreach ($nameParts as $namePart) {
+        if ($namePart != $nameParts[0]) {
+            $stringsAfterFirstName[] = $namePart;
+        }
+    }
+
+    return implode(" ", $stringsAfterFirstName);
+}
+?>
